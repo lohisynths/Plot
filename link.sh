3f2a3a5fb6505db6759a97963bb0f40b5558fb84
lohisynths <lohisynths@gmail.com>
2017-04-04 23:23:44 +0200

    midified link.sh

--- link.sh
+++ link.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 set -x
 
-rm -rf ../libs/*
+rm -rf libs/*
 
-make -j
-cp libPlot.a ../libs/libPlot.a
 
-cd ../libs
+make -C Debug clean
+make -j -C Debug
+
+cp Debug/libPlot.a libs/libPlot.a
+
+#TODO: replace absolute paths
+cd libs
 ar -x /usr/lib/x86_64-linux-gnu/libboost_filesystem.a
 ar -x /usr/lib/x86_64-linux-gnu/libboost_system.a
 ar -x /usr/lib/x86_64-linux-gnu/libboost_iostreams.a
 ar -x libPlot.a
-ar -qc librap.a *.o
-
+ar -qc libGnuPlot.a *.o
 rm *.o
 rm libPlot.a
+cd ../
